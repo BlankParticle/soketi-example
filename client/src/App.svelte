262ca004964dabd16452636217491b867ea899e5
BlankParticle <blankparticle@gmail.com>
2024-03-09 21:44:35 +0530

    feat: some styles

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -32,4 +32,46 @@
 </main>
 
 <style>
+  :global(body, html) {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :global(:root) {
+    background-color: #0b0b0b;
+    color: #fff;
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  }
+
+  main {
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  button {
+    padding: 3px 4px;
+    width: 100px;
+    border: none;
+    outline: none;
+    background-color: aquamarine;
+    font-weight: bold;
+    font-size: larger;
+  }
+  div {
+    border: 1px solid #fff;
+  }
 </style>
